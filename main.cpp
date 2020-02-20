3db36bde33a63ceca092f6d3a254a052d468ef20
Adam Walker <adamwalker567123@gmail.com>
2020-02-20 15:20:53 -0800

    I just broke stuff

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
     cout << "I am making some changes that don't break the code" << endl;
+    cout << "This line is missing a semicolon and will break the code!" << endl
     return 0;
 }
