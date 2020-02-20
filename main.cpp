28a0b642b84fdbeb6992eb4071fde641da28543d
Adam Walker <adamwalker567123@gmail.com>
2020-02-20 15:26:52 -0800

    Revert "Added anotherFunction() to code"

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void anotherFunction() {
-    cout << "everybody this is some super important functionality I just added";
-}
-
 int main() {
-    anotherFunction();
-
     cout << "Hello world" << endl;
     return 0;
 }
